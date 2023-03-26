9073311fa5dec4075eb0937fcbfd9b6ba8822bc8
tsapap <stefano.pappalardo@yahoo.it>
2023-03-26 22:20:17 +0200

    Add files via upload

---
+++ webhook.php
@@ -0,0 +1,13 @@
+<?php
+// Original Answer
+header('Content-Type: application/json');
+$request = file_get_contents('php://input');
+$req_dump = print_r( $request, true );
+$fp = file_put_contents( 'request.log', $req_dump );
+
+// Updated Answer
+if($json = json_decode(file_get_contents("php://input"), true)){
+   $data = $json;
+}
+print_r($data);
+?>
